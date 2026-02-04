16d44a46bc38b0e96c76db01628adee373edc891
EddieTheDog <1dylanroeder1@gmail.com>
2026-02-03 16:43:15 -0800

    Update [id].js

--- functions/api/guest/[id].js
+++ functions/api/guest/[id].js
@@ -1,7 +1,7 @@
 export async function onRequestGet({ params, env }) {
-  const { results } = await env.DB.prepare(`
-    SELECT * FROM guests WHERE id = ?
-  `).bind(params.id).all();
+  const { results } = await env.DB.prepare(
+    `SELECT * FROM guests WHERE id = ?`
+  ).bind(params.id).all();
 
   if (!results.length) {
     return new Response("Not found", { status: 404 });
