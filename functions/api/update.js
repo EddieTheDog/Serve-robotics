d53ae0c62bb75c0abe0ec0ca8a020bc5b75ab621
EddieTheDog <1dylanroeder1@gmail.com>
2026-02-03 16:43:33 -0800

    Update update.js

--- functions/api/update.js
+++ functions/api/update.js
@@ -2,8 +2,7 @@ export async function onRequestPost({ request, env }) {
   const { guestId, status } = await request.json();
 
   await env.DB.prepare(`
-    UPDATE guests
-    SET status = ?, updated_at = ?
+    UPDATE guests SET status = ?, updated_at = ?
     WHERE id = ?
   `).bind(status, Date.now(), guestId).run();
 
