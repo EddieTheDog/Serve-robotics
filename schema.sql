7c426bdef9f08ce59629ceb6c7969218cef77d91
EddieTheDog <1dylanroeder1@gmail.com>
2026-02-07 10:08:38 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS guests (
   id TEXT PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
+  qr_data TEXT,
   status TEXT NOT NULL,
   created_at INTEGER NOT NULL,
   updated_at INTEGER NOT NULL
